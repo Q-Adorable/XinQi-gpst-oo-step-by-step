35043385df3310cf5675823aa6e82285b355b2f4
XinQi <xinqi@CNxinqi.lan>
2018-07-17 21:21:18 +0800

    gpst commit

--- main/practice-2/class.js
+++ main/practice-2/class.js
@@ -1,2 +1,9 @@
 // Write your code here
-module.exports = class Class {}
+module.exports = class Class {
+    constructor(number){
+        this.number=number;
+    }
+    assignLeader(student){
+        this.leader=student.name;
+    }
+}
--- main/practice-3/class.js
+++ main/practice-3/class.js
@@ -1,2 +1,16 @@
 // Write your code here
-module.exports = class Class {}
+module.exports = class Class {
+    constructor(number){
+        this.number=number;
+    }
+    assignLeader(student){
+        this.leader=student.name;
+        if(this.hasStudent === true){
+            return 'Assign team leader successfully.';
+        }
+        return 'It is not one of us.'
+    }
+    appendMember(student){
+        this.hasStudent=true;
+    }
+}
